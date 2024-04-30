422627eef9a5628ff55d7d7f05a185386d16673b
matmor88 <matteo.morato@mail.polimi.it>
2024-04-30 13:30:44 +0200

    synchronized Round

--- CodexNaturalis/src/main/java/it/polimi/ingsw/Controller/RoundController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Controller/RoundController.java
@@ -9,20 +9,26 @@ public class RoundController {
     private static int round = -1;
     private Random rand = new Random();
 
-    RoundController(Player[] players, int numPlayers) {
-        if(this.players==null) {
-            this.players = Arrays.copyOf(players, numPlayers);
+    public RoundController(Player[] players, int numPlayers) {
+        synchronized (players) {
+            if (this.players == null) {
+                this.players = Arrays.copyOf(players, numPlayers);
+            }
         }
     }
     public void nextRound(){
-        if(round== players.length-1)
-            round=0;
-        else
-            round++;
+        synchronized (players) {
+            if (round == players.length - 1)
+                round = 0;
+            else
+                round++;
+        }
     }
     public void setFirstPlayer(){
-        if(round==-1) {
-            round = rand.nextInt(players.length);
+        synchronized (players) {
+            if (round == -1) {
+                round = rand.nextInt(players.length);
+            }
         }
     }
     public Player getCurrentPlayer(){
