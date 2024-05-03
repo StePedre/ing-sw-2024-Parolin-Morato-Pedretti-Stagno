d9dd0d8b486c274dea90b915ee68dbbd51d65f65
silviaparolin <silvia.parolin@mail.polimi.it>
2024-05-03 13:52:31 +0200

    test class

---
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/Model/GameTest.java
@@ -0,0 +1,28 @@
+package it.polimi.ingsw.Model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void start() {
+    }
+
+    @Test
+    void finish() {
+    }
+
+    @Test
+    void createGlobalChat() {
+    }
+
+    @Test
+    void createPrivateChats() {
+    }
+
+    @Test
+    void isFirst() {
+    }
+}
