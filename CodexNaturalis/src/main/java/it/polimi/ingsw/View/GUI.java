6c8b78975c4c256fadd3ea15482bf3e167159f5d
silviaparolin <silvia.parolin@mail.polimi.it>
2024-05-09 10:10:44 +0200

    new scene controllers in GUI started

--- CodexNaturalis/src/main/java/it/polimi/ingsw/View/GUI.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/View/GUI.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.View;
 
+import it.polimi.ingsw.Model.Hand;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,6 +9,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.event.*;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
@@ -17,19 +20,30 @@ import java.util.Objects;
 public class GUI extends Application {
 
     @Override
-    public void start(Stage nickStage) throws IOException {
+    public void start(Stage Stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/insertNick.fxml"));
-        nickStage.setTitle("Codex Naturalis");
-        nickStage.setScene(new Scene(root, 350, 300));
-        nickStage.setFullScreen(true);
-        nickStage.show();
+        Stage.setTitle("Codex Naturalis");
+        Stage.setScene(new Scene(root, 350, 300));
+        Stage.setFullScreen(true);
+        Stage.show();
+        // prima volta che si carica il playground:
+        Parent root2 = FXMLLoader.load(getClass().getResource("/playerground.fxml"));
+        // aggiungere la hand
+        Stage.setTitle("Codex Naturalis");
+        Stage.setScene(new Scene(root2, 350, 300));
+        Stage.setFullScreen(true);
+        Stage.show();
     }
 
     public static void main(String[] args){
         GUI.launch();
     }
 
-
+    public void loadHand(Hand hand){
+        HBox handZone = new HBox(37.5);  // distanza tra le carte
+       // handZone.setPadding(new Insets(20));
+       // handZone.getChildren().addAll(handCardleft, buttonL, handCardCenter, button2, handCardRight, button3);
+    }
 
 }
 
