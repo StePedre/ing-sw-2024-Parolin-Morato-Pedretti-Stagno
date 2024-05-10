e0789f5c67841a349b9547127b040ac00fddbab3
silviaparolin <silvia.parolin@mail.polimi.it>
2024-05-10 09:25:36 +0200

    error fixed in gui controller

--- CodexNaturalis/src/main/java/it/polimi/ingsw/View/DrawController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/View/DrawController.java
@@ -23,37 +23,37 @@ public class DrawController {
         goldFaceDown.setImage(new Image("src/main/resources/CODEX_cards_gold_back/" + decks[1].getCards().get(2).getId()));
     }
     public int chooseCard(){
-        int choice = -1;
+        int[] choice = new int[] { -1 };
         DropShadow dropShadow = new DropShadow();
         dropShadow.setRadius(5);
         dropShadow.setColor(javafx.scene.paint.Color.BLUE);
 
         resFaceDown.setOnMouseClicked(event -> {
-            choice = 0;
+            choice[0] = 0;
             resFaceDown.setEffect(dropShadow);
         });
         resFaceUp1.setOnMouseClicked(event -> {
-            choice = 1;
+            choice[0] = 1;
             resFaceUp1.setEffect(dropShadow);
             buttonSubDraw.setDisable(false);
         });
         resFaceUp2.setOnMouseClicked(event ->{
-            choice = 2;
+            choice[0] = 2;
             resFaceUp2.setEffect(dropShadow);
             buttonSubDraw.setDisable(false);
         });
         goldFaceDown.setOnMouseClicked(event ->{
-            choice = 3;
+            choice[0] = 3;
             goldFaceDown.setEffect(dropShadow);
             buttonSubDraw.setDisable(false);
         });
         goldFaceUp1.setOnMouseClicked(event -> {
-            choice = 4;
+            choice[0] = 4;
             goldFaceUp1.setEffect(dropShadow);
             buttonSubDraw.setDisable(false);
         });
         goldFaceUp2.setOnMouseClicked(event-> {
-            choice = 5;
+            choice[0] = 5;
             goldFaceUp2.setEffect(dropShadow);
             buttonSubDraw.setDisable(false);
         });
@@ -62,7 +62,7 @@ public class DrawController {
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.close();
         });
-        return choice;
+        return choice[0];
     }
 }
 
