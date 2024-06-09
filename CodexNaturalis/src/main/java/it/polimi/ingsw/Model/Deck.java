d2cc088ce8f77687e17ef0d5fb658b751e4e1b7e
matmor88 <matteo.morato@mail.polimi.it>
2024-06-09 21:25:55 +0200

    deck adjustment

--- CodexNaturalis/src/main/java/it/polimi/ingsw/Model/Deck.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Model/Deck.java
@@ -77,7 +77,7 @@ public class Deck implements Serializable {
      * a negative number or a number bigger than current deck's size.
      */
     public PlayableCard drawCard(int position) {
-        if (position >= 0 && position < 3) {
+        if (position >= 0 && position < cards.size()) {
             return cards.remove(position);
         } else {
             throw new IndexOutOfBoundsException("Invalid position: " + position);
